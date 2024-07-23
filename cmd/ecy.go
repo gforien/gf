2f8bafe91aef35bccb457c307c963878a24a312e
Gabriel Forien <gforien@users.noreply.github.com>
2024-07-23 09:12:30 +0200

    feat: ecy (#1)

---
+++ cmd/ecy.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
+)
+
+var ecyCmd = &cobra.Command{
+	Use:   "ecy",
+	Short: "Exa configuration generator from YAML",
+	Long:  `Usage: gf ecy`,
+	Run:   Ecy,
+}
+
+var EcyPath = fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".dotfiles/common/config/exa/colors.yaml")
+
+type Data struct {
+	Entries []Entry `yaml:"exacolors"`
+}
+
+type Entry struct {
+	Ansi        string   `yaml:"ansi"`
+	Identifiers []string `yaml:"iden"`
+}
+
+func Ecy(cmd *cobra.Command, args []string) {
+	var ecyFile []byte
+	ecyFile, err := os.ReadFile(EcyPath)
+	if err != nil {
+		log.Fatalf("error '%s' when reading ecy config file '%s'", err, EcyPath)
+	}
+
+	var out Data
+	if err := yaml.Unmarshal(ecyFile, &out); err != nil {
+		log.Fatal("error unmarshalling data: ", err)
+	}
+
+	for _, entry := range out.Entries {
+		for _, identifier := range entry.Identifiers {
+			fmt.Printf("%s=%s:", identifier, entry.Ansi)
+		}
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(ecyCmd)
+}
---
+++ cmd/root.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gf",
+	Short: "gf cli",
+	Long: `gf cli`,
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gf.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
