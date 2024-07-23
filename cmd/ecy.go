00e5e2bf618fd692083aaec7e0d12a394a6ae588
gforien <gforien+dev@gmail.com>
2024-07-23 09:06:02 +0200

    chore: cleanup

--- cmd/ecy.go
+++ cmd/ecy.go
@@ -9,42 +9,36 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// ecyCmd represents the ecy command
 var ecyCmd = &cobra.Command{
 	Use:   "ecy",
 	Short: "Exa configuration generator from YAML",
-	Long: `Usage: gf ecy [command]
-
-Available commands:
-  debug     print details about ecy configuration
-  read      parse configuration
-  generate  generate exa configuration`,
-	Args:      cobra.MatchAll(cobra.MaximumNArgs(1), cobra.OnlyValidArgs),
-	ValidArgs: []string{"debug", "read", "generate"},
-	Run:       run,
+	Long:  `Usage: gf ecy`,
+	Run:   Ecy,
 }
 
-var ecyPath = fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".dotfiles/common/config/exa/colors.yaml")
+var EcyPath = fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".dotfiles/common/config/exa/colors.yaml")
 
 type Data struct {
 	Entries []Entry `yaml:"exacolors"`
 }
+
 type Entry struct {
-	Ansi       string   `yaml:"ansi"`
+	Ansi        string   `yaml:"ansi"`
 	Identifiers []string `yaml:"iden"`
 }
 
-func run(cmd *cobra.Command, args []string) {
+func Ecy(cmd *cobra.Command, args []string) {
 	var ecyFile []byte
-	ecyFile, err := os.ReadFile(ecyPath)
+	ecyFile, err := os.ReadFile(EcyPath)
 	if err != nil {
-		log.Fatalf("error '%s' when reading file '%s'", err, ecyPath)
+		log.Fatalf("error '%s' when reading ecy config file '%s'", err, EcyPath)
 	}
 
 	var out Data
 	if err := yaml.Unmarshal(ecyFile, &out); err != nil {
 		log.Fatal("error unmarshalling data: ", err)
 	}
+
 	for _, entry := range out.Entries {
 		for _, identifier := range entry.Identifiers {
 			fmt.Printf("%s=%s:", identifier, entry.Ansi)
@@ -52,27 +46,6 @@ func run(cmd *cobra.Command, args []string) {
 	}
 }
 
-// func read(in string, out []byte) error {
-//   var err error
-//   outFile, err := os.ReadFile(in)
-//   out = outFile
-//   return err
-// }
-//
-// func parse(in byte[], out interface{}) error {
-//   return yaml.Unmarshal(in, out)
-// }
-
 func init() {
 	rootCmd.AddCommand(ecyCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ecyCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// ecyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -11,15 +11,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gf",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "gf cli",
+	Long: `gf cli`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -33,10 +26,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gf.yaml)")
 
 	// Cobra also supports local flags, which will only run
