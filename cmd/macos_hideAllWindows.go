3126bf4807845a375bab608947d0db9808afff8c
gforien-externe <gforien.externe@bedrockstreaming.com>
2024-10-03 08:58:06 +0200

    feat: macos/hideAllWindows

---
+++ cmd/macos_hideAllWindows.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+// hideAllWindowsCmd represents the hideAllWindows command
+var hideAllWindowsCmd = &cobra.Command{
+	Use:   "hideAllWindows",
+	Short: "Hide all windows except kitty",
+	Run:   hideAllWindows,
+}
+
+func hideAllWindows(cobraCmd *cobra.Command, args []string) {
+	cmd := exec.Command(
+		"osascript",
+		"-e",
+		`tell application "System Events" to set visible of every process whose visible is true and name is not "Kitty" to false`,
+	)
+	err := cmd.Run()
+	cobra.CheckErr(err)
+}
+
+func init() {
+	macosCmd.AddCommand(hideAllWindowsCmd)
+}
